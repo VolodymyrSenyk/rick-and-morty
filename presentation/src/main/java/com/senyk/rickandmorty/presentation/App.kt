979e56bda46445d46c464c941d4fa0e5c0f1cb94
VolodymyrSenyk <senikvladimir@gmail.com>
2025-08-30 11:11:52 +0300

    Add StrictMode

--- presentation/src/main/java/com/senyk/rickandmorty/presentation/App.kt
+++ presentation/src/main/java/com/senyk/rickandmorty/presentation/App.kt
@@ -1,7 +1,29 @@
 package com.senyk.rickandmorty.presentation
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setUpStrictMode()
+    }
+
+    private fun setUpStrictMode() {
+        if (!BuildConfig.DEBUG) return
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .build()
+        )
+    }
+}
